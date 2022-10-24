9087f3dc72ab3da96010408a70527dec947e5c83
nazaroviktor2 <nazarov20012004@gmail.com>
2022-10-24 09:11:55 +0300

    Удален токен

--- 2110/.send.py
+++ 2110/.send.py
@@ -1,17 +1,16 @@
 import asyncio
-import sys
-from aiogram import Bot, Dispatcher, types, executor
+from aiogram import Bot, Dispatcher
 from aiogram.types import InputFile
 
-bot = Bot(token = "5333115681:AAGXfCQUp97Xwbaa7ufDVhTkNU7UPubXCjc")
+bot = Bot(token = "YOUR_TELEGRAM_BOT_TOKEN")
 dp = Dispatcher(bot)
-NAZAROFF_ID= "581853528"
+USER_ID= "YOUR_TELEGRAM_USER_ID"
 
 async def send_photo():
     # old style:
     # await bot.send_message(message.chat.id, message.text)
     file_path=f"./photos/last.jpg"
-    await bot.send_photo(chat_id=NAZAROFF_ID, photo=InputFile(file_path))
+    await bot.send_photo(chat_id=USER_ID, photo=InputFile(file_path))
 
 
 loop = asyncio.get_event_loop()
